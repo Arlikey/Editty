8bdbb3e94ff1efba2598135328d38e755b93e64b
admin <admin@DESKTOP-FA5KDQG>
2024-10-21 17:53:00 +0300

    Add project files.

---
+++ Models/Editor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Editty.Models
+{
+    public class Editor
+    {
+        public IEnumerable<FontFamily> FontFamilies { get; set; }
+
+        public Editor()
+        {
+            FontFamilies = Fonts.SystemFontFamilies;
+        }
+    }
+}
