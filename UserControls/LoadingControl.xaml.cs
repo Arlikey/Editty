59f329f0f4d1f218a96cdaa1d8c830c60a8d3e37
admin <admin@DESKTOP-FA5KDQG>
2024-10-24 18:15:43 +0300

    commited while something bad isn't happened

---
+++ UserControls/LoadingControl.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Editty.UserControls
+{
+    /// <summary>
+    /// Interaction logic for LoadingControl.xaml
+    /// </summary>
+    public partial class LoadingControl : UserControl
+    {
+        private BackgroundWorker _backgroundWorker = new BackgroundWorker();
+        public LoadingControl()
+        {
+            InitializeComponent();
+            this._backgroundWorker.WorkerReportsProgress = true;
+            _backgroundWorker.WorkerSupportsCancellation = true;
+            _backgroundWorker.DoWork += _backgroundWorker_DoWork;
+            _backgroundWorker.ProgressChanged += _backgroundWorker_ProgressChanged;
+        }
+
+        private void _backgroundWorker_ProgressChanged(object? sender, ProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void _backgroundWorker_DoWork(object? sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker backgroundWorker = sender as BackgroundWorker;
+            if (backgroundWorker != null)
+            {
+                for (int i = 0; i <= 100;  i++) {
+                    if (backgroundWorker.CancellationPending == true)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                    else
+                    {
+                        Thread.Sleep(250);
+                        backgroundWorker.ReportProgress(i);
+                    }
+                }
+            }
+        }
+    }
+}
--- ViewModels/EditorViewModel.cs
+++ ViewModels/EditorViewModel.cs
@@ -8,6 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
